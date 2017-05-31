0558d5afa30b70fd559f8b326a9641c27f749756
bob <bob@example.com>
2017-05-31 22:46:15 +0800

    test3.2

--- metapb.pb.go
+++ metapb.pb.go
@@ -17,12 +17,12 @@ import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
 
-// Reference imports to suppress errors if they are not otherwise used.
+// Reference imporaaaaaaaaaaaaaats to suppress errors if they are not otherwise used.
 var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
 
-// This is a compile-time assertion to ensure that this generated file
+// This is a compile-time assertaaaaaaion to ensure that this generated file
 var lcxlcxlcx_ = fmt.Errorf
 var _ = fmt.Errorf
 // is compatilcxlcxlcxble with the proto package it is being compiled against.
